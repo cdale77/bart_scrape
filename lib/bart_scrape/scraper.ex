fa4f541e610b11e170f2298d4c0b2c1d7743b2c6
Brad Johnson <cdale77@gmail.com>
2017-10-30 16:17:16 -0700

    fixed genserver

--- lib/bart_scrape/scraper.ex
+++ lib/bart_scrape/scraper.ex
@@ -7,16 +7,17 @@ defmodule BartScrape.Scraper do
   end
 
   def init(state) do
-    schedule_work()
+    #schedule_work()
     {:ok, state}
   end
 
-  def handle_info(:work) do
-    schedule_work()
+  def handle_info(:work, state) do
+    #schedule_work()
     Fetcher.fetch_data |> Parser.parse_data |> Recorder.record_delays
+    {:noreply, state}
   end
 
-  defp schedule_work() do
-    Process.send_after(self(), :work, 0.25 * 60 * 60 * 1000) # 0.25 hrs
-  end
+  #defp schedule_work() do
+  #   Process.send_after(self(), :work, 0.25 * 60 * 60 * 1000) # 0.25 hrs
+  #end
 end
