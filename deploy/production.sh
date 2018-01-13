17c069a8998eed66d830d1f10613cfd2f157b032
Brad Johnson <cdale77@gmail.com>
2018-01-12 17:18:19 -0800

    deploy script

--- deploy/production.sh
+++ deploy/production.sh
@@ -6,16 +6,17 @@ eval $(aws ecr get-login --no-include-email --region us-east-1)
 
 # Set up build variables
 echo "Setting build variables"
-ecr_repo=$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/bart_scrape
+ecr_repo=$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/bart-scrape
+tag=$ecr_repo:latest
 
 # Build the container and tag it with the sha
 echo "Building the container"
 aws s3 cp s3://bart-scrape/prod.secret.exs config/prod.secret.exs
-docker build -t latest .
+docker build -t $tag .
 
 # Push the container to ECR
 echo "Pushing the container"
-docker push $ecr_repo:latest
+docker push $tag
 
 # Set the compose tasks
 AWS_REGION=us-east-1 \
