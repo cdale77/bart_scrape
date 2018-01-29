0ad133812688f4908b582ade5a7e9e3046cd5fbb
Brad Johnson <cdale77@gmail.com>
2018-01-29 13:33:31 -0800

    simplify for heroku

--- lib/bart_scrape/scraper.ex
+++ lib/bart_scrape/scraper.ex
@@ -1,28 +1,10 @@
 defmodule BartScrape.Scraper do
-  use GenServer
   require Logger
   alias BartScrape.{Fetcher, Parser, Recorder}
 
-  def start_link do
-    GenServer.start_link(__MODULE__, %{})
-  end
 
-  def init(state) do
-    Logger.info "Starting Scraper"
-    #schedule_work()
-    {:ok, state}
-  end
-
-  def handle_info(:work, state) do
+  def look_for_delays do
     Logger.info "Looking for delays"
-    # hack for heroku scheduler.
-    #schedule_work()
     Fetcher.fetch_data |> Parser.parse_data |> Recorder.record_delays
-    {:noreply, state}
-  end
-
-  defp schedule_work() do
-    interval = 100
-    Process.send_after(self(), :work, interval)
   end
 end
